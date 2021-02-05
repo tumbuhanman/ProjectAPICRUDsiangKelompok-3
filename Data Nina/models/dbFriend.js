bdf7928353f04153c54c3a428751de15568eb573
Nina <ninashahab25@gmail.com>
2021-02-05 18:09:30 +0700

    database

---
+++ Data Nina/models/dbFriend.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+       id : "123",
+       userId : "nina25",
+       name : "Nina",
+    },
+    {
+        id : "456",
+       userId : "winda38",
+       name : "Winda",
+    },
+    {
+        id : "567",
+       userId : "Yanuar88",
+       name : "Yanuar",
+    }
+
+]
---
+++ Data Nina/models/dbTransaction.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+       id : "111",
+       userId : "nina25",
+       friendId : "123",
+       nominal : "100.000",
+    },
+    {
+        id : "222",
+       userId : "winda38",
+       friendId : "456",
+       nominal : "200.000",
+    },
+    {
+        id : "333",
+       userId : "Yanuar88",
+       friendId : "567",
+       nominal : "2.000.000",
+    }
+]
