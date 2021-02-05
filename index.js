cb2123c91019231073bdb2f885be336a2ce825f7
ikhwanwan07 <kenthoz07@gmail.com>
2021-02-05 15:19:49 +0700

    struktur dasar node js done- ikhwan

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const route = require('./route/route')
+const port = 3000
+
+app.use(route)
+app.use(function(error,req,res,next) {
+    console.log(error)
+    res.status(500).send(error,message)
+})
+app.listen(port, () => {
+  console.log(`Server on at http://localhost:${port}`)
+})
---
+++ route/route.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express.Router()
+
+//ikhwan
+app.get('/',(req,res) => {
+    res.send("welcome")
+})
+// winda
+
+// 
+
+//wildan
+
+// 
+
+//yanuar
+
+//
+
+
+
+module.exports = app
