d432f7c3946ec25433f49fa6e29c6cbffeb0ca10
Yanuar Aji Candra <yanuarcandra24@gmail.com>
2021-02-07 12:07:46 +0700

    fixing data validation

--- route/itemsRoute.js
+++ route/itemsRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express.Router()
 const items = require('../models/dbItem')
+const users = require('../models/dbUser')
 
 function getItem(param) {
     const item = items.find(item => item.id === param)
@@ -12,15 +13,28 @@ function getItemIndex(param) {
     return index
 }
 
+function getUserId(param) {
+    const user = users.filter(user => user.id === param)
+    return user
+}
+
+function dbStructure(a, b, c) {
+    return {
+        id: a,
+        userId: b,
+        name: c
+    }
+}
+
 app.get('/item', (req, res) => {
     res.send(items)
 })
 
 app.get('/item/:id', (req, res) => {
-    const id = req.params.id
+    const param = req.params.id
 
-    if (getItem(id)) {
-        res.send(getItem(id))
+    if (getItem(param)) {
+        res.send(getItem(param))
     }
 
     else {
@@ -29,35 +43,46 @@ app.get('/item/:id', (req, res) => {
 })
 
 app.post('/item', (req, res) => {
-    const data = req.body
-    const id = data.id
+    const { id, userId, name } = req.body
 
     if (getItem(id)) {
-        res.status(400).send("There is similar Id, please change")
+        res.status(400).send("There is similar Id, please change the input value")
     }
 
-    else if (Object.keys(data).length === 0) {
-        res.status(400).send("Please write something")
+    else if (getUserId(userId).length == 0) {
+        res.status(400).send("There is no similar userId, please change the input value")
     }
 
     else {
-        items.push(data)
-        res.send(data)
+        if (id && userId && name) {
+            items.push(dbStructure(id, userId, name))
+            res.send(dbStructure(id, userId, name))
+        }
+
+        else {
+            res.status(400).send("Please check your input")
+        }
     }
 })
 
 app.put('/item/:id', (req, res) => {
-    const id = req.params.id
-    const data = req.body
+    const param = req.params.id
+    const { id, userId, name } = req.body
 
-    if (getItem(id)) {
-        if (Object.keys(data).length === 0) {
-            res.status(400).send("Please write something")
+    if (getItem(param)) {
+        if (getUserId(userId).length == 0) {
+            res.status(400).send("There is no similar userId, please change the input value")
         }
 
         else {
-            items[getItemIndex(id)] = data
-            res.send(data)
+            if (id && userId && name) {
+                items[getItemIndex(param)] = dbStructure(id, userId, name)
+                res.send(dbStructure(id, userId, name))
+            }
+
+            else {
+                res.status(400).send("Please write something")
+            }
         }
     }
 
@@ -67,10 +92,10 @@ app.put('/item/:id', (req, res) => {
 })
 
 app.delete('/item/:id', (req, res) => {
-    const id = req.params.id
+    const param = req.params.id
 
-    if (getItem(id)) {
-        const deletedItem = items.splice(getItemIndex(id), 1)
+    if (getItem(param)) {
+        const deletedItem = items.splice(getItemIndex(param), 1)
         res.send(deletedItem)
     }
 
