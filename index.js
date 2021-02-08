b5d6c892a5efc33dd61297fd34d0da3a68032eb2
Yanuar Aji Candra <yanuarcandra24@gmail.com>
2021-02-08 09:08:16 +0700

    commit

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const app = express()
 const route = require('./route/route')
 const transaction = require('./route/transactionRoute')
 const item = require('./route/itemsRoute')
-const port = 3000
 const friends = require('./route/friendsRoute')
+const port = 3000
 
 app.use(express.json())
 app.use(route, transaction, item, friends)
-app.use(function(error,req,res,next) {
-     console.log(error)
-    res.status(500).send(error,message)
- })
+
+app.use(function (error, req, res, next) {
+  console.log(error)
+  res.status(500).send("Error : ", error.message)
+})
+
 app.listen(port, () => {
   console.log(`Server on at http://localhost:${port}`)
 })
